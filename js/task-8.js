859e974de1286c910ca94b64c960b153017380ec
savchenko-maksym <maxim.savchenko91@gmail.com>
2024-12-21 17:03:47 +0200

    add

--- js/task-8.js
+++ js/task-8.js
@@ -1,2 +1,15 @@
 // Напишіть функцію, яка розверає масив у зворотньому напрямку.Метод reverse() не використовувати
-// const arrNumber = [ 11, 9, 2, 1 ]
+
+const arrNumber = [11, 9, 2, 1];
+
+function backarr(arr) {
+  const newArr = [];
+  for (let i = arr.length - 1; i >= 0; i -= 1) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
+
+console.log(backarr(arrNumber));
+
+console.log(arrNumber.toReversed());
--- js/task-9.js
+++ js/task-9.js
@@ -1,3 +1,17 @@
 // Напишіть функцію, яка очистить масив від небажаних значень, таких як false, undefined, порожні рядки, нуль, null
 
 // Результат, що очікується[0, 1, false, 2, undefined, '', 3, null] => [1, 2, 3]
+
+function clear(arr) {
+  const newArr = [];
+  for (const element of arr) {
+    if (element) {
+      newArr.push(element);
+    }
+  }
+  return newArr;
+}
+
+console.log(clear([0, 1, false, 2, undefined, "", 3, null]));
+
+console.log([0, 1, false, 2, undefined, "", 3, null, 7].filter((x) => x > 5));

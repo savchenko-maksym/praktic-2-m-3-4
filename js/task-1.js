65909f60885323239e259bf3c3769091b6afe58a
savchenko-maksym <maxim.savchenko91@gmail.com>
2024-12-21 14:44:20 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+// todo Напиши скрипт, який для об'єкта user послідовно:
+//   - додасть поле mood зі значенням 'happy'
+//   - замінить hobby на 'skydiving'
+//   - замінить значення premium на false
+//   - виводить вміст об'єкта user у форматі ключ:значення,
+// використовуючи Object.keys() і for...of
+
+const user = {
+  name: 'John',
+  age: 20,
+  hobby: 'tennis',
+  premium: true,
+}
---
+++ js/task-4.js
@@ -0,0 +1,9 @@
+// todo Напишіть функцію calcTotalPrice(stones, stonesName), яка приймає масив об'єктів та рядок із назвою каменя.
+// Функція рахує і повертає загальну вартість каменів з таким іменем, ціною та кількістю з об'єкта
+
+const stonesArray = [
+  { name: 'Ізумруд', price: 1300, quantity: 4 },
+  { name: 'Брилліант', price: 2700, quantity: 6 },
+  { name: 'Сапфір', price: 400, quantity: 7 },
+  { name: 'Щебінь', price: 150, quantity: 100 },
+]
